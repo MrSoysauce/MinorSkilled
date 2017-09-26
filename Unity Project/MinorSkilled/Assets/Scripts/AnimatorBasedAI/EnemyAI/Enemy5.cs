b59a85fdee8c1c7b6bc184ce66483310446dce59
LEONB\Leon <lajbrands@outlook.com>
2017-09-26 23:10:15 +0200

    + Dropping enemy

---
+++ Unity Project/MinorSkilled/Assets/Scripts/AnimatorBasedAI/EnemyAI/Enemy5.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy5 : Enemy2
+{
+    [SerializeField] private float descendSpeed;
+    [SerializeField] private Transform descendEnd;
+    private Vector3 startDescend;
+
+    private bool hanging = true;
+
+    protected override void Start()
+    {
+        startDescend = transform.position;
+        base.Start();
+    }
+
+    public void StartFalling()
+    {
+        StartCoroutine(fall());
+    }
+
+    protected override void Update()
+    {
+        if (!hanging)
+        {
+            base.Update();
+        }
+
+        animator.SetBool("Hanging", hanging);
+    }
+
+    private IEnumerator fall()
+    {
+        float t = 0;
+        while (t <= 1)
+        {
+            t += Time.deltaTime * descendSpeed;
+            transform.position = Vector3.Lerp(startDescend, descendEnd.position, t);
+
+            inRangeCheck();
+            if (attached)
+                yield break;
+
+            yield return null;
+        }
+
+        visuals.transform.localRotation = Quaternion.identity;
+
+        hanging = false;
+    }
+}
