3f6633dd95a76d772e8d928a07a4a174df30aeb0
Kurisu <chrissi.cano@yahoo.de>
2017-10-18 13:40:03 +0200

    *scripts and stuff

---
+++ Unity Project/MinorSkilled/Assets/Scripts/Flicker.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flicker : MonoBehaviour {
+
+	private Light flickering;
+	public bool active;
+	public float interval;
+	public Vector2 randomInterval;
+
+	// Use this for initialization
+	void Start () {
+		flickering = this.GetComponent<Light> (); 
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (active) 
+		{
+			// Goes back and forth between the two forever
+			if (flickering.enabled == true) 
+			{
+				flickering.enabled = false;
+				StartCoroutine ("FlickerIntervalRandom");
+			}
+		} 
+		else 
+		{
+			if (flickering.enabled == true)
+			{
+				flickering.enabled = false;
+				StartCoroutine ("FlickerInterval", interval);
+			}
+		}
+	}
+
+	IEnumerator FlickerInterval()
+	{
+		yield return new WaitForSeconds(interval * Time.deltaTime * 10f);
+		flickering.enabled = true;
+	}
+
+	IEnumerator FlickerIntervalRandom()
+	{
+		yield return new WaitForSeconds(Random.Range(randomInterval.x, randomInterval.y) * Time.deltaTime * 10f);
+		flickering.enabled = true;
+	}
+
+}
---
+++ Unity Project/MinorSkilled/Assets/Scripts/Teleport.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+	public GameObject exit;
+	public GameObject player;
+
+	void OnCollisionEnter (Collision collision)
+	{
+		if(collision.gameObject == player)
+		{
+			player.transform.position = exit.transform.position;
+		}
+	}
+}
