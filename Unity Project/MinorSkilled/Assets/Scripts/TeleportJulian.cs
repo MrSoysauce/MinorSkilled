9cbe5a5876f924b60c287d4f96348c7ba9cf13ab
howlmortar <juliandealdrey@gmail.com>
2017-10-18 13:34:41 +0200

    asdfgh

---
+++ Unity Project/MinorSkilled/Assets/Scripts/TeleportJulian.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportJulian : MonoBehaviour {
+
+    public GameObject receiver;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.localPosition = receiver.transform.localPosition;
+        }
+    }
+}
